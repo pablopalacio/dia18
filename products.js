0c2c49719e717804ac7785bb9fd4d8f2b7b9c9f6
Pablop <palaciopablo92@gmail.com>
2025-07-24 17:25:50 -0300

    products

---
+++ products.js
@@ -0,0 +1,8 @@
+export const products = [
+  { id: 1, nombre: "Waffle con frutas", precio: 6.5, categoria: "Waffle" },
+  { id: 2, nombre: "Crème Brûlée", precio: 7.0, categoria: "Postre" },
+  { id: 3, nombre: "Mix de Macarons (5)", precio: 8.0, categoria: "Macaron" },
+  { id: 4, nombre: "Tiramisú clásico", precio: 5.5, categoria: "Tiramisú" },
+  { id: 5, nombre: "Baklava de pistacho", precio: 4.0, categoria: "Baklava" },
+  { id: 6, nombre: "Panna Cotta de fresa", precio: 8.5, categoria: "Panna Cotta" },
+];
